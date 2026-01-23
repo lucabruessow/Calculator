26049934d3e951a3bdc72653cc4c9f9a381b96f9
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2026-01-23 10:45:01 +0100

    feat: add basic calculator operations

---
+++ js.js
@@ -0,0 +1,10 @@
+const funcs = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
+}
+
+function operate(a, b, op) {
+    funcs[op](a, b);
+}
