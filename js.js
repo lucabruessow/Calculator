4e05a17c3a0d80cbde3e51e0813a1af3edab3c8c
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2026-01-28 14:58:38 +0100

    feat: handle button events and store inputs

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
+const buttons = document.querySelector(".calc__buttons");
+const display = document.querySelector(".calc__display");
+
 const funcs = {
     '+': (a, b) => a + b,
     '-': (a, b) => a - b,
@@ -6,5 +9,28 @@ const funcs = {
 }
 
 function operate(a, b, op) {
-    return funcs[op](a, b);
+    return funcs[op](+a, +b);
 }
+
+function handlePointerDown() {
+    let var1 = "";
+    let var2 = "";
+    let op = null;
+
+    return function(event) {
+        const tar = event.target;
+        const content = event.target.textContent;
+
+        if (op === null && tar.matches(".calc__button--number")) { 
+            var1 += content;
+        }
+        if (op === null && tar.matches(".calc__button--operator")) {
+            op = content;
+        }
+        if (op !== null && tar.matches(".calc__button--number")) {
+            var2 += content;
+        }
+    }
+}
+
+buttons.addEventListener("pointerdown", handlePointerDown());
